e29097f2607824dfe2f070d922985a66c01fbb79
Rajdeep Singh <radewala.2@gmail.com>
2024-02-03 18:32:10 +0530

    config the withNextVideo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts',"mp4","vtt"],
 
-export default nextConfig;
+};
+
+export default withNextVideo(nextConfig);
